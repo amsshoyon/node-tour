3e7493411de9c41197a5a7e9aa57e53b4f7329a7
Abdullah Al mamun <abdullah.mamun@sharetrip.net>
2021-05-03 03:24:43 +0600

    ///

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,6 @@
 // review / rating / createdAt / ref to tour / ref to user
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema({
     review: {
@@ -30,6 +31,8 @@ const reviewSchema = new mongoose.Schema({
     toObject: { virtuals: true }
 });
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true }); 
+
 reviewSchema.pre(/^find/, function (next) {
     // this.populate({
     //   path: 'tour',
@@ -55,19 +58,40 @@ reviewSchema.statics.calcAverageRatings = async function(tourId) {
         {
             $group: {
                 _id: '$tour',
-                mRating: {$sum: 1},
+                nRating: {$sum: 1},
                 avgRating: {$avg: '$rating'}
             }
         }
     ])
-    console.log(stats);
+    // console.log(stats);
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nRating,
+            ratingsAverage: stats[0].avgRating
+        });
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4.5
+        });
+    }
 }
 
-reviewSchema.pre('save', function (next) {
+reviewSchema.post('save', function(next) {
     // this points to current review
     this.constructor.calcAverageRatings(this.tour);
-    next();
 })
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+    this.r = await this.findOne();
+    // console.log(this.r);
+    next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+    // await this.findOne(); does NOT work here, query has already executed
+    await this.r.constructor.calcAverageRatings(this.r.tour);
+});
 
 const Review = mongoose.model('Review', reviewSchema);
 
